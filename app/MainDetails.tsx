7b432113762e0da6bcf32e41ac26e260e6681cbd
Azran Hossain <azranhossain@outlook.com>
2024-10-04 17:40:58 +0600

    bottom skew added

--- app/MainDetails.tsx
+++ app/MainDetails.tsx
@@ -11,14 +11,19 @@ const MainDetails = () => {
                 <div className='w-[190px] h-[190px] bg-[#E5E7EC] border-[5px] rounded-xl rotate-6 border-white ml-6 z-10' />
                 <div className='w-[190px] h-[190px] bg-[#E5E7EC] border-[5px] rounded-xl -rotate-6 border-white mt-5' />
             </section>
-            <div className="relative bg-white mt-60">
+
+            <section className="relative bg-white mt-60 pb-10">
                 <div className="absolute -top-7 left-0 right-0 h-20 bg-white transform skew-y-[8deg]"></div>
                 <article className="relative z-1 pt-14">
                     <ProjectShowCase />
                     <ServiceShowCase />
                     <ServicesList />
                 </article>
-            </div>
+            </section>
+
+            <section className="relative bg-[#222] h-[600px]">
+                <div className="absolute -top-7 left-0 right-0 h-20 bg-white transform -skew-y-[8deg] border-b-[20px] border-[#9DFF50]"></div>
+            </section>
         </div>
     );
 };
