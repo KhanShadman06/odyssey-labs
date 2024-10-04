f20af37ba8ca5bf2c79f0ce37ece34fccb420d85
Azran Hossain <azranhossain@outlook.com>
2024-10-04 17:23:41 +0600

    small project skew added

--- app/MainDetails.tsx
+++ app/MainDetails.tsx
@@ -1,20 +1,26 @@
-import ProjectShowCase from "./ProjectShowCase"
-import ServiceShowCase from "./ServiceShowCase"
-import ServicesList from "./ServicesList"
-import TopSection from "./TopSection"
+import ProjectShowCase from "./ProjectShowCase";
+import ServiceShowCase from "./ServiceShowCase";
+import ServicesList from "./ServicesList";
+import TopSection from "./TopSection";
 
 const MainDetails = () => {
     return (
         <div>
-
             <TopSection />
-            <article className="bg-white">
-                <ProjectShowCase />
-                <ServiceShowCase />
-                <ServicesList />
-            </article>
+            <section className="absolute z-10 mt-10 flex">
+                <div className='w-[190px] h-[190px] bg-[#E5E7EC] border-[5px] rounded-xl rotate-6 border-white ml-6 z-10' />
+                <div className='w-[190px] h-[190px] bg-[#E5E7EC] border-[5px] rounded-xl -rotate-6 border-white mt-5' />
+            </section>
+            <div className="relative bg-white mt-60">
+                <div className="absolute -top-7 left-0 right-0 h-20 bg-white transform skew-y-[8deg]"></div>
+                <article className="relative z-1 pt-14">
+                    <ProjectShowCase />
+                    <ServiceShowCase />
+                    <ServicesList />
+                </article>
+            </div>
         </div>
-    )
-}
+    );
+};
 
-export default MainDetails
+export default MainDetails;
